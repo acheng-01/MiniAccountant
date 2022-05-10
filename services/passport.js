e7a225cb353a16987cb7b34c0c3d0d4306e8812f
Andrew Cheng <password>
2022-05-09 19:18:01 -0700

    added proxy settings to passport

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         async (accessToken, refreshToken, profile, done) => {
             const existingUser = await authService.findUserByGoogleId(profile.id);
@@ -45,7 +46,8 @@ passport.use(
         {
             clientID: keys.facebookClientID,
             clientSecret: keys.facebookClientSecret,
-            callbackURL: '/auth/facebook/callback'
+            callbackURL: '/auth/facebook/callback',
+            proxy: true
         },
         async (accessToken, refreshToken, profile, done) => {
             const existingUser = await authService.findUserByFacebookId(profile.id);
